0db6afdfe7551fb1204522ba4ad406915506da06
deniseprosser <denise.prosser23@gmail.com>
2024-10-06 13:56:17 +0100

    Added useEffect to fix forecast bug, removed conversion functionality

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,50 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import "./WeatherTemperature.css";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function fahrenheit() {
-    return (props.celsius * 9) / 5 + 32;
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="WeatherTemperature">
-        <p className="temperature-value">
-          {Math.round(props.celsius)}{" "}
-          <span className="temperature-unit">
-            °C |{" "}
-            <a href="/" onClick={showFahrenheit}>
-              °F
-            </a>
-          </span>
-        </p>
-      </div>
-    );
-  } else {
-    return (
-      <div className="WeatherTemperature">
-        <p className="temperature-value">
-          {Math.round(fahrenheit())}{" "}
-          <span className="temperature-unit">
-            <a href="/" onClick={showCelsius}>
-              °C
-            </a>{" "}
-            | °F
-          </span>
-        </p>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <p className="temperature-value">
+        {Math.round(props.celsius)} <span className="temperature-unit">°C</span>
+      </p>
+    </div>
+  );
 }
